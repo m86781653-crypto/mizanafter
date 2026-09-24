02b9ba21d44c8cef3fd199813bfba3e6b5b73ba1
m86781653-crypto <m86781653@gmail.com>
2026-09-24 22:53:59 +0300

    feat(offline): enable app shell offline support

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { registerOfflineSupport } from './lib/registerOffline';
+
+registerOfflineSupport();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
